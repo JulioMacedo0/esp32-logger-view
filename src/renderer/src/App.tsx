e851a2502a71d7ca6b56671caba569dd9a9f73f4
Julio Macedo <juliocesar1076@gmail.com>
2024-12-04 11:30:54 -0300

    feat: auto detect esp32 and connect

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,44 @@
 import Versions from './components/Versions'
 import electronLogo from './assets/electron.svg'
+import { SerialPort } from 'serialport'
+import { useEffect, useRef } from 'react'
 
 function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+  const port = useRef<SerialPort>(null)
+
+  const ConnetToEsp32 = async (): Promise<void> => {
+    const ports = await SerialPort.list()
+
+    const myDevice = ports.find((port) => port.vendorId === '10c4' && port.productId === 'ea60')
+
+    if (myDevice) {
+      const serialPort = new SerialPort(
+        { path: myDevice.path, baudRate: 115200, lock: false },
+        (err) => {
+          if (err) {
+            console.log(err)
+
+            return
+          }
+          console.log(`Connedted in ${myDevice.manufacturer} on port ${myDevice.path}`)
+        }
+      )
+
+      serialPort.on('data', (data) => console.log(`data from esp32:${data}`))
+      serialPort.on('close', () => {
+        console.log('Device disconnected, try to connect...')
+        setTimeout(ConnetToEsp32, 750)
+      })
+      port.current = serialPort
+    } else {
+      console.log('Device not found, try to connect..')
+      setTimeout(ConnetToEsp32, 750)
+    }
+  }
+
+  useEffect(() => {
+    ConnetToEsp32()
+  }, [])
 
   return (
     <>
@@ -16,16 +52,34 @@ function App(): JSX.Element {
         Please try pressing <code>F12</code> to open the devTool
       </p>
       <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
+        <button
+          className="action"
+          onClick={() =>
+            port.current.write('ON\n', (err) => {
+              if (err) {
+                console.log(err)
+                return
+              }
+              console.log('ok')
+            })
+          }
+        >
+          ON
+        </button>
+        <button
+          className="action"
+          onClick={() =>
+            port.current.write('OFF\n', (err) => {
+              if (err) {
+                console.log(err)
+                return
+              }
+              console.log('ok')
+            })
+          }
+        >
+          OFF
+        </button>
       </div>
 
       <Versions></Versions>
