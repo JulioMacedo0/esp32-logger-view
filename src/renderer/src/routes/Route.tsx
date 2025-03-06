f0924a54ccc434e128d516ea3a83b38ae907fc5b
Julio Macedo <juliocesar1076@gmail.com>
2025-03-06 10:40:26 -0300

    feat: use hash routers

--- src/renderer/src/routes/Route.tsx
+++ src/renderer/src/routes/Route.tsx
@@ -1,13 +1,13 @@
 import { Home } from '@renderer/screens/Home/Home'
 
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { HashRouter, Routes, Route } from 'react-router'
 
 export function Router(): JSX.Element {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
