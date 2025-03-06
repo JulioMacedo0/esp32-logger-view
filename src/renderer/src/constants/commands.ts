2dcf17c9a7a84da854df90738a324e2da4c1f1ec
Julio Macedo <juliocesar1076@gmail.com>
2025-03-06 10:40:04 -0300

    feat: add new responses

--- src/renderer/src/constants/commands.ts
+++ src/renderer/src/constants/commands.ts
@@ -2,7 +2,7 @@ export const START_BYTE = 0x02
 export const END_BYTE = 0x03
 
 export type COMMAND = 'FORWARD' | 'REVERSE' | 'STOP' | 'PING' | 'PONG'
-export type RESPONSE = 'PONG'
+export type RESPONSE = 'PONG' | 'CMD_DETECTION_ON' | 'CMD_DETECTION_OFF'
 
 export const COMMANDS: Record<COMMAND, number> = {
   FORWARD: 0x10,
@@ -13,5 +13,7 @@ export const COMMANDS: Record<COMMAND, number> = {
 } as const
 
 export const RESPONSES: Record<RESPONSE, number> = {
-  PONG: 0x14
+  PONG: 0x14,
+  CMD_DETECTION_ON: 0x15,
+  CMD_DETECTION_OFF: 0x16
 } as const
